4965f34f48d8c32a06b98c21942549c4f0ecfbb1
MuhammadBilal0111 <m.bilal0111@gmail.com>
2024-08-22 17:40:22 -0700

    adding comment model

---
+++ Blog Website/server/Model/commentsModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const commentSchema = mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    postId: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+    numOfLikes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timeStamps: true }
+);
+const Comment = mongoose.model("Comment", commentSchema);
+module.exports = Comment;
