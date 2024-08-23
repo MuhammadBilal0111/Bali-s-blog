393fab952af28b563f4b4648fe93a86754bd85f7
MuhammadBilal0111 <m.bilal0111@gmail.com>
2024-08-22 17:31:07 -0700

    create comment-section

--- Blog Website/Client/blog/src/Components/CommentSection.jsx
+++ Blog Website/Client/blog/src/Components/CommentSection.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { Textarea, Button } from "flowbite-react";
+
 function CommentSection({ postId }) {
+  const [comments, setComments] = useState("");
   const { currentUser } = useSelector((state) => state.user);
+  const handleOnChangeText = (e) => {
+    setComments(e.target.value);
+  };
+  const handleSubmitForm=async (e)=>{
+    
+  }
   return (
     <div className="max-w-2xl w-full mx-auto">
       {currentUser ? (
@@ -15,16 +24,42 @@ function CommentSection({ postId }) {
           />
           <span>
             @
-            <Link className="hover:underline text-cyan-600 italic" to="/dashboard?tab=profile">
+            <Link
+              className="hover:underline text-cyan-600 italic"
+              to="/dashboard?tab=profile"
+            >
               {currentUser.data.email}
             </Link>
           </span>
         </div>
       ) : (
-        <div className="flex justify-center">
+        <div className="flex gap-1 text-sm text-teal-500 my-5 justify-center">
           <p>You must be signed in to see the comments</p>
-          <Link to="/sign-in">Tap to Sign In</Link>
+          <Link className="hover:underline text-cyan-600 italic" to="/sign-in">
+            Tap to Sign In
+          </Link>
         </div>
+      )}
+      {currentUser && (
+        <form onSubmit={handleSubmitForm} className="border border-6 p-4 rounded-xl border-teal-500">
+          <Textarea
+            id="comment"
+            placeholder="Leave a comment..."
+            required
+            maxLength="200"
+            value={comments}
+            onChange={handleOnChangeText}
+            rows="4"
+          />
+          <div className="flex justify-between items-center p-2 mt-2 md:flex-row flex-col gap-3">
+            <p className="text-sm text-gray-600">
+              {200 - comments.length} letters remaining
+            </p>
+            <Button type="submit" gradientDuoTone="purpleToPink" outline>
+              Submit
+            </Button>
+          </div>
+        </form>
       )}
     </div>
   );
